0c1ed78e80835dd440a1ddfaa46f9b710660d418
superjom <superjom@gmail.com>
2013-02-22 22:06:43 +0800

    update setup.py
    move swinf.py to core.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from swinf import swinf
+from swinf import core
 
 setup(name='swinf',
-    version='%s.%s.%s' % swinf.__version__,
+    version='%s.%s.%s' % core.__version__,
     description='WSGI micro web framework',
     author='Chunwei Yan',
     author_email='yanchunwei@outlook.com',
     url='http://github.com/superjom/swinf',
     
-    py_modules=['swinf.swinf', 'swinf.selector'],
+    py_modules=['swinf.core', 'swinf.selector'],
     license='MIT',
 )
