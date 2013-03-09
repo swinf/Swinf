653bea5707c958feed4770a7df37d7ddcd48bb18
superjom <superjom@gmail.com>
2013-03-10 00:46:58 +0800

    add template

---
+++ swinf/template.py
@@ -0,0 +1,207 @@
+import re
+import os
+from swinf import HTTPError, lazy_attribute
+from swinf.utils.html import html_escape
+from swinf.utils.text import touni
+from swinf.utils.functional import cached_property
+from swinf.debug import deco
+
+class TemplateError(HTTPError):
+    pass
+
+# cache compiled templates
+TEMPLATES = {}
+
+class BaseTemplate:
+    """
+    Base class and minimal API for template adapters
+    """
+    extensions = ['', 'tpl', 'shtml']
+    settings = {}
+    defaults = {}   #used in render
+
+    def __init__(self, source=None, name=None, lookup=[], encoding='utf8', **settings):
+        """
+        args:
+            source:     File or template source
+            name:       
+        """
+        self.filename = name
+        self.source = source.read() if hasattr(source, 'read') else source
+        self.encoding = encoding
+        self.settings = self.settings.copy()
+        self.settings.update(settings)
+        # search template file 
+        if not self.source and self.filename:
+            self.filename = self.sarch(self.name, self.lookup)
+            if not self.filename:
+                raise TemplateError("Template %s not found") % repr(self.name)
+            if not self.source and self.filename:
+                raise TemplateError("No template specified.")
+        self.prepare(settings)
+
+    @classmethod
+    def search(cls, name, lookup=[]):
+        for _dir in lookup:
+            filename = os.path.join(_dir, name)
+            for extension in cls.extensions:
+                if os.path.isfile(filename + extension):
+                    return filename + extension
+
+    def prepare(self, **options):
+        raise NotImplementedError
+
+    def render(self, *args, **kwargs):
+        raise NotImplementedError
+
+
+class SimpleTemplate(BaseTemplate):
+    blocks = ('if', 'elif', 'else', 'try', 'except', 'finally', \
+              'for', 'while', 'with', 'def', 'class')
+    dedent_blocks = ('elif', 'else', 'except', 'finally')
+    
+    @lazy_attribute
+    def re_pytokens(cls):
+        return re.compile(r"""
+            (''(?!')|""(?!")|'{6}|"{6}      # Empty strings    
+            |'(?:[^\\']|\\.)+?'             #   '
+            |"(?:[^\\"]|\\.)+?"             #   "
+            |'{3}(?:[^\\]|\\.|\n)+?{3}'     #   '
+            |"{3}(?:[^\\]|\\.|\n)+?{3}"     #   "
+            |\#.*)""", re.VERBOSE)
+
+    def prepare(self, noescape=False, escape_func=html_escape):
+        self._str = lambda x: touni(x, self.encoding)
+        self._escape = escape_func
+        if noescape:
+            self._str, self._escape = self._escape, self._str
+
+    @cached_property
+    def co(self):
+        return compile(self.code, self.filename or '<string>', 'exec')
+    
+    @cached_property
+    def code(self):
+        stack = []
+        ptrbuffer = []
+        codebuffer = []
+        multiline = dedent = oneline = False
+        template = self.source or open(self.filename, 'rb').read()
+        indent_space = ' ' * 4
+
+        def yield_tokens(line):
+            for i, part in enumerate(re.split(r'\{\{(.*?)\}\}', line)):
+                if i % 2:
+                    if part.startswith('!'): yield 'RAW', part[1:]
+                    else: yield 'CMD', part
+                else: yield 'TXT', part
+
+        def flush():
+            if not ptrbuffer: return
+            cline = []
+            for line in ptrbuffer:
+                for token, value in line:
+                    if token == 'TXT': cline.append(repr(value)) # add '
+                    elif token == 'RAW': cline.append('_str(%s)' % value)
+                    elif token == 'CMD': cline.append('_escape(%s)' % value)
+                    cline.append(', ')
+                cline[-1] = cline[-1][:-2] + r'\n'
+            cline = ''.join(cline)[:-2]
+            cline = '_printlist([' + cline + '])'
+            del ptrbuffer[:]
+            code(cline)
+
+        def code(stmt):
+            for line in stmt.splitlines():
+                codebuffer.append(indent_space * len(stack) + line.strip())
+
+        # run
+        for lineno, line in enumerate(template.splitlines(True)):
+            line = touni(line, self.encoding)
+            sline = line.lstrip()
+            print 'sline: ', sline
+            if sline and sline.startswith('%%'):
+                line = sline[2:].lstrip() # line following the %%
+                cmd = re.split(r'[^a-zA-Z0-9_]', line)[0]
+                flush()
+
+                if cmd in self.blocks or multiline:
+                    cmd = multiline or cmd
+                    dedent = cmd in self.dedent_blocks
+                    # a single line
+                    if dedent and not oneline and not multiline:
+                        cmd = stack.pop()
+                    code(line)
+                    oneline = not line.endswith(':')
+                    multiline = cmd if line.endswith('\\') else False
+                    if not oneline and not multiline:
+                        stack.append(cmd)
+                elif cmd == 'end' and stack:
+                    code('#end(%s) %s' % (stack.pop(), line.strip()[3:]))
+                else:
+                    code(line)
+        flush()
+        return '\n' .join(codebuffer) + '\n' 
+
+    @deco
+    def execute(self, _stdout, *args, **kwargs):
+        for dictarg in args: kwargs.update(dictarg)
+        env = self.defaults.copy()
+        # TODO add more template function here
+        env.update({'_stdout': _stdout,
+            '_printlist': _stdout.extend,
+            '_escape': self._escape,
+            'get': env.get,
+            'setdefault': env.setdefault,
+            'defined': env.__contains__
+        })
+        env.update(kwargs)
+        print 'env', env
+        eval(self.co, env)
+        return env
+
+    @deco
+    def render(self, *args, **kwargs):
+        for dictarg in args: kwargs.update(dictarg)
+        stdout = []
+        self.execute(stdout, kwargs)
+        print 'stdout: ', stdout
+        return ''.join(stdout)
+
+
+TEMPLATE_PATH = ['./views']
+from swinf import DEBUG, abort
+
+@deco
+def template(*args, **kwargs):  
+    '''
+    Get a rendered template as a string iterator.
+    args[0]     : default to source 
+    name        : template filename
+    source      : template source
+    '''
+    source = args[0] if args else None
+    name = kwargs.pop('name', None)
+    print '.. source:', source
+    print '.. name:', name
+    if not source and name:
+        abort("No Template Specified")
+    adapter = kwargs.pop('adapter', SimpleTemplate)
+    lookup = kwargs.pop('lookup', TEMPLATE_PATH)
+    # TODO tplid can be abs path or source
+    tplid = (id(lookup), name or source)
+    print "adapter:", adapter
+    print "lookup", lookup
+    print "tplid:", tplid
+    # refresh template if DEBUG and load changes every time
+    if tplid not in TEMPLATES or DEBUG:
+        settings = kwargs.pop('settings', {})
+        if isinstance(source, adapter):
+            TEMPLATES[tplid] = source
+            if settings: TEMPLATES[tplid].prepare(**settings)
+        else:
+            TEMPLATES[tplid] = adapter(source=source, name=name, lookup=lookup, **settings)
+    if not TEMPLATES[tplid]:
+        abort(500, 'Template (%s) not found' % name)
+    for dictarg in args[1:]: kwargs.update(dictarg)
+    return TEMPLATES[tplid].render(kwargs)
