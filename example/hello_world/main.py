29c3c99fbe87438ebcec02fe815bb653572a1081
superjom <superjom@gmail.com>
2013-03-11 19:56:24 +0800

    add static file auto support

--- example/hello_world/main.py
+++
@@ -1,9 +0,0 @@
-from swinf import core
-from swinf.selector import handler_walk
-import environ
-
-core.DEBUG = True
-core.bind_environ(environ.ENV)
-
-handler_walk("controller/")
-core.run()
--- swinf/conf/project_frame/main.py
+++ swinf/conf/project_frame/main.py
@@ -1,7 +1,36 @@
+import os
 import swinf
-from swinf.selector import handler_walk
+from swinf.selector import handler_walk, route
 
 handler_walk("controller/")
+
+# ------------ default handlers here ----------------------
+# ------------ you can change them ------------------------
+static_dir = os.path.join(os.getcwd(), 'view/static')
+@route("/static/:path")
+def static(path):
+    return swinf.send_file(path, root=static_dir)
+
+@route("/images/:path")
+def images(path):
+    return swinf.send_file(path, root=os.path.join(static_dir, 'images'))
+
+@route("/files/:path")
+def files(path):
+    return swinf.send_file(path, root=os.path.join(static_dir, 'files'))
+
+@route("/style/:path")
+def style(path):
+    return swinf.send_file(path, root=os.path.join(static_dir, 'style'))
+
+@route("/script/:path")
+def script(path):
+    return swinf.send_file(path, root=os.path.join(static_dir, 'script'))
+
+
+# --------------- your code here --------------------------
+
+
 
 if __name__ == '__main__':
     swinf.run()
