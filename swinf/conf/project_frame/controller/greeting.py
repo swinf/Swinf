d155565123eaf7ed18be806865356933695d3a30
superjom <superjom@gmail.com>
2013-03-16 17:39:52 +0800

    update project controller example

--- swinf/conf/project_frame/controller/greeting.py
+++ swinf/conf/project_frame/controller/greeting.py
@@ -6,11 +6,6 @@ __handlespace__ = {}
 bind_environ(__handlespace__)
 
 
-@handler()
+@handler("GET")
 def hello():
     return "<h1>Hello World!</h1>"
-
-@handler()
-def static():
-    return send_file("index.html", root="/home/chunwei/swinf/example/hello_world/view")
-
