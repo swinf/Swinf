459f4fd0c996f9237c66fba351848949dc99bc82
superjom <superjom@gmail.com>
2013-03-23 14:26:21 +0800

    add default handlers

---
+++ swinf/utils/default_handlers.py
@@ -0,0 +1,25 @@
+import os
+import swinf
+from swinf.selector import route
+# ------------ you can change them ------------------------
+static_dir = os.path.join(os.getcwd(), swinf.config.template.static_file_path)
+@route("/static/:path")
+def static(path):
+    return swinf.send_file(path, root=static_dir)
+
+@route("/images/:path")
+def images(path):
+    return swinf.send_file(path, root=os.path.join(static_dir, 'images'))
+
+@route("/files/:path")
+def files(path):
+    return swinf.send_file(path, root=os.path.join(static_dir, 'files'))
+
+@route("/style/:path")
+def style(path):
+    return swinf.send_file(path, root=os.path.join(static_dir, 'style'))
+
+@route("/script/:path")
+def script(path):
+    return swinf.send_file(path, root=os.path.join(static_dir, 'script'))
+
